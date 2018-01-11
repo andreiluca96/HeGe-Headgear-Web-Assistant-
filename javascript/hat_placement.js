4da6d0ab083c24603b24d97af279fe2037fddd90
andrluc <andrluc@amazon.com>
2018-01-11 21:03:42 +0200

    Changed the face recogintion API and placed the hat

---
+++ javascript/hat_placement.js
@@ -0,0 +1,27 @@
+var videoInput = document.getElementById('inputVideo');
+
+var ctracker = new clm.tracker();
+ctracker.init();
+ctracker.start(videoInput);
+
+var canvasInput = document.getElementById('overlay');
+var cc = canvasInput.getContext('2d');
+function drawLoop() {
+    var currentHatDiv = document.getElementsByClassName('hat-item-active')[0];
+    var currentHatImg = currentHatDiv.childNodes[0];
+    requestAnimFrame(drawLoop);
+    cc.clearRect(0, 0, 320, 240);
+    if (ctracker.getCurrentPosition()) {
+        ctracker.draw(overlay);
+        var positions = ctracker.getCurrentPosition();
+        console.log(positions);
+        cc.drawImage(currentHatImg, positions[14][0] - (positions[0][0] - positions[14][0])*0.3, positions[0][1], (positions[0][0] - positions[14][0]) * 1.4, (positions[0][1] - positions[7][1]) * 1.3);
+    }
+}
+drawLoop();
+
+function positionLoop() {
+    requestAnimationFrame(positionLoop);
+
+}
+positionLoop();
